8dba9ce9ba0f5e9bad2648db4dac07f8dd7c69d7
Erik Mellum <emellum@mail.csuchico.edu>
2015-10-02 13:11:08 -0700

    Fixed semicolon

--- js/app.js
+++ js/app.js
@@ -25,7 +25,7 @@ var app = angular.module('myApp', ['ui.router'])
       }
   }
   };
-});
+})
 .directive('ngEnter', function() {
     return function(scope, element, attrs) {
         element.bind("keydown keypress", function(event) {
